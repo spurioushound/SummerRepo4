0c9b61f5553d98009b2850fe5fb91c3e01f5f0da
spurioushound <stephen.hornbeck@gmail.com>
2023-03-29 15:44:44 -0400

    Update DS413613ClassworkLabForkingRfile.R

--- DS413613ClassworkLabForkingRfile.R
+++ DS413613ClassworkLabForkingRfile.R
@@ -2,14 +2,14 @@ library(tidyverse)
 library(ggplot2)
 library(dplyr)
 mpg
-# My name is John Doe
-# My favorite color is blue
+# My name is Stephen Hornbeck
 # This is my coding that generates Boxplots for transmission types
+# The data source is the MPG table
 ggplot(data = mpg) +
   geom_boxplot(mapping = aes(x = drv, y = displ, fill = drv)) +
   ggtitle("Boxplots (mpg data : transmission types vs displacement)") +
   xlab("transmission types") +
-  ylab("displacement") 
+  ylab("displacement")  + coord_flip()
   
 
 
